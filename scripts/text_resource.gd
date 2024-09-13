0b3059095ce8b837faeceff9b978d982cca36382
Fidgety48 <fidgetplayz48@gmail.com>
2024-09-13 23:38:10 +0200

    added a file for text strings for the adventure (text_resource.gd)

---
+++ scripts/text_resource.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+var dialogue : Dictionary = {
+	"txt1_intro":"You are inside of a room-temperature room, that smells and looks exactly like a room. From this, you conclude that you are in a room.",
+	"txt1_search":"You notice that there's something within your [b]view,[/b] however, you can't quite make out what it is."
+}
