a2e73565b90687c52b2157deba329efbf2815c22
Fidgety48 <fidgetplayz48@gmail.com>
2024-09-13 22:47:46 +0200

    get rid off that dumbass pass bro

--- branches/message_enter.gd
+++ branches/message_enter.gd
@@ -5,13 +5,10 @@ extends LineEdit
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_text_submitted(new_text: String) -> void:
 	var command = new_text.split(' ')[0]
 	var target = " ".join(new_text.split(" ").slice(1))
-	pass # Replace with function body.
