c077acfb2b5c840f43f3e9a6cfd522b4a23bf19c
Fidgety48 <fidgetplayz48@gmail.com>
2024-09-14 13:24:05 +0200

    fixed text_resource.gd, made it global and added a script to textdisplay

--- scripts/text_resource.gd
+++ scripts/text_resource.gd
@@ -1,6 +1,9 @@
-extends Resource
+extends Node
 
 var dialogue : Dictionary = {
+	"txt_credits":"A Game made by RubyPiec and Fidgety48",
+	
+	
 	"txt1_intro":"You are inside of a room-temperature room, that smells and looks exactly like a room. From this, you conclude that you are in a room.",
 	"txt1_search":"You notice that there's something within your [b]view,[/b] however, you can't quite make out what it is."
 }
---
+++ scripts/textdisplay.gd
@@ -0,0 +1,8 @@
+extends RichTextLabel
+
+
+func _ready() -> void:
+	text = textres.dialogue["txt1_intro"]
+
+func _process(delta: float) -> void:
+	pass
