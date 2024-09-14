b02290a7ffb4457cdf9c21c1b312fd6e78cf078e
Fidgety48 <fidgetplayz48@gmail.com>
2024-09-14 17:00:05 +0200

    implemented commands and attahc command

--- scripts/message_enter.gd
+++ scripts/message_enter.gd
@@ -1,6 +1,5 @@
 extends LineEdit
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,8 +11,22 @@ func _process(delta: float) -> void:
 func _on_text_submitted(new_text: String) -> void:
 	var command = new_text.split(' ')[0]
 	var target = new_text.split(" ").slice(1)
-	self.text = ""
+	text = ""
+	if command == "credits":
+		$"../textdisplay".attach(command, textres.dialogue["txt_credits"])
+	
 	if command == "view":
 		print('view') # Something so it doesn't display an error. remove later
 		# add view to the screen
+	if command in ["examine", "search", "look", "search"]:
+		if command == "look" and target == "around":
+			print(new_text)
+		else:
+			print(new_text)
+
+	if command in ["use", "utilize", "operate"]: #general use
+		print(new_text)
+		
+	if command in ["use", "consume", "eat"]: #food/edible
+		print(new_text)
 	#Pseudocode: if command=="examine" && room.contains(target): print(description[target])
--- scripts/textdisplay.gd
+++ scripts/textdisplay.gd
@@ -1,8 +1,5 @@
 extends RichTextLabel
 
 
-func _ready() -> void:
-	text = textres.dialogue["txt1_intro"]
-
-func _process(delta: float) -> void:
-	pass
+func attach(command: String, attachment: String) -> void:
+	text = text + "\n\n> " + command + "\n\n" + attachment #example: First line \n > hello! i am the text! \n hello this is a response.
