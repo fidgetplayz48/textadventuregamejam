3fc3c6a7c68c1914b7abcad3740b700b9a620692
RubyPiec <50266387+RubyPiec@users.noreply.github.com>
2024-09-14 18:44:33 +0200

    added movement, a map, and examine
    no functionality for examine yet

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,3 +1,17 @@
 extends Node2D
 
 var inventory = [] #max 8 items  (?)
+
+
+var playerLoc = [0,0] # Spawns the player at 0,0 (x,y). x=0 y=0 at topleft
+	
+# Defines the map. Quick overview of things you can put in objects:
+# canGo - decides what directions the player can go, "r"-right,"u"-up,"d"-down,"l"-left
+# objects - defines the objects in the room
+var map = [ 
+	[{"canGo":["r","d"],"objects":["something"]},{},{},{}],
+	[{},{},{},{}],
+	[{},{},{},{}],
+	[{},{},{},{}]
+]
+#Note: when checking the map, y goes first then x
--- scripts/message_enter.gd
+++ scripts/message_enter.gd
@@ -1,9 +1,11 @@
 extends LineEdit
 
+
 var bannedLetters = ["a"]
 #other nodes
 @onready var textDisplay = $"../textdisplay"
 @onready var soundSys = $"../generalSoundPlayer"
+@onready var mainNode = $".."
 
 func _ready() -> void:
 	pass
@@ -19,8 +21,9 @@ func _on_text_changed(new_text: String) -> void:
 			caret_column = caretPos #load caret pos so it doesnt fuck up eehehh
 
 func _on_text_submitted(new_text: String) -> void:
+	var currentTile = mainNode.map[mainNode.playerLoc[1]][mainNode.playerLoc[0]] # Gets the current tile of the player
 	var command = new_text.split(' ')[0]
-	var target = new_text.split(" ").slice(1)
+	var target = Array(new_text.split(" ").slice(1))
 	soundSys.playSound("enter")
 	text = ""
 	if command == "credits":
@@ -29,15 +32,33 @@ func _on_text_submitted(new_text: String) -> void:
 	if command == "view":
 		print('view') # Something so it doesn't display an error. remove later
 		# add view to the screen
-	if command in ["examine", "search", "look", "search"]:
-		if command == "look" and target == "around":
-			print(new_text)
-		else:
-			print(new_text)
-
+	if command in ["examine", "search", "look", "inspect"]:
+		if target.size()>0: #If array is empty the whole thing crashes lmao
+			if command == "look" and target[0] == "around":
+				print(new_text)
+			else:
+				for i in target:
+					if(currentTile["objects"].has(i)):
+					#If the tile at the player location has any of the words in the target. Fix later to only allow 1
+						print("examination is legal!")
+		
+		
 	if command in ["use", "utilize", "operate"]: #general use
 		print(new_text)
 		
 	if command in ["use", "consume", "eat"]: #food/edible
 		print(new_text)
-	#Pseudocode: if command=="examine" && room.contains(target): print(description[target])
+		
+	#Movement:
+	if command in ["down","south"]:
+		if(currentTile["canGo"].has("d")):
+			mainNode.playerLoc[1]+=1 #Adds 1 to the player's y, moving them down
+	if command in ["up","north"]:
+		if(currentTile["canGo"].has("u")):
+			mainNode.playerLoc[1]-=1 #ditto, but opposite
+	if command in ["right","east"]:
+		if(currentTile["canGo"].has("r")):
+			mainNode.playerLoc[0]+=1 #ditto but moves right instead on x axis
+	if command in ["left","west"]:
+		if(currentTile["canGo"].has("l")):
+			mainNode.playerLoc[0]-=1 #ditto, but opposite
