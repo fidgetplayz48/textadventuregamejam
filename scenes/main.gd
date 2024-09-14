72358219a759dd2ac28fbf9668bb45be154145b4
Fidgety48 <fidgetplayz48@gmail.com>
2024-09-14 22:54:53 +0200

    Update main.gd

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 var inventory = [] #max 8 items  (?)
 
-
 var playerLoc = [0,0] # Spawns the player at 0,0 (x,y). x=0 y=0 at topleft
 	
 # Defines the map. Quick overview of things you can put in objects:
