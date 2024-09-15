a5f885e19242477ac12951b29916582231254e99
Fidgety48 <fidgetplayz48@gmail.com>
2024-09-15 21:48:42 +0200

    relinks

--- scripts/view.gd
+++ scripts/view.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
 @onready var mainNode = $".."
+var isReady = false
+
+func _ready():
+	isReady = true
 
 func _on_visibility_changed() -> void:
-	pass#_update()
+	if !isReady:
+		return
+	_update()
 
 func _update():
+	for node in children():
+		if node is Sprite2D:
+			if node.name != "base"
 	mainNode.map[mainNode.playerLoc[1]][mainNode.playerLoc[0]]["objects"]
