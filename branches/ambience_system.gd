47cd9bbbe137aa74a55353a7802df6a9a4df5736
Fidgety48 <fidgetplayz48@gmail.com>
2024-09-15 18:11:47 +0200

    (UNSTABLE)

--- branches/ambience_system.gd
+++ branches/ambience_system.gd
@@ -1,11 +1,10 @@
 extends AudioStreamPlayer
 
+@export_range(0.01,1.0) var distIntensity := 0.1 ##the closer this value is to 1, the less it distorts.
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if !playing:
+		return
+	var zzz = (Time.get_ticks_msec()*0.2*delta)
+	var distortion = abs((sin(zzz)*(0.258*sin(zzz*2.7))+cos(zzz)**2+1)*1.4*sin(zzz*0.277))
+	$".".pitch_scale = lerp(distortion+0.1, 1.0, distIntensity)
--- scripts/view.gd
+++ scripts/view.gd
@@ -3,4 +3,7 @@ extends Node2D
 @onready var mainNode = $".."
 
 func _on_visibility_changed() -> void:
+	_update()
+
+func _update():
 	mainNode.map[mainNode.playerLoc[1]][mainNode.playerLoc[0]]["objects"]
